c3fd072ba40cec9ccf9b3fca8357e60037bc597e
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-03-30 16:25:11 +0000

    added tag model

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'core.apps.CoreConfig',
     'user.apps.UserConfig',
+    'recipy.apps.RecipyConfig',
 
 ]
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
+
+def sample_user(email='test@test.com', password='123testing'):
+    return get_user_model().objects.create_user(email, password)
 
 class ModelTests(TestCase):
 
@@ -29,3 +33,11 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_superuser('zoki@zoki.com', '123testing')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        # Test the tag string representation
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
