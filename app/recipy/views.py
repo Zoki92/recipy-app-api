22b34c7e40fca1f40ca812f4cc1d385d00f00dda
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-03-30 18:01:45 +0000

    added functionality to add tag

--- app/recipy/views.py
+++ app/recipy/views.py
@@ -5,7 +5,9 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Tag
 from recipy import serializers
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     # Manage tags in the database
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -13,6 +15,11 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        return self.queryset.filter(user=self.request.user)
+                            .order_by('-name')
+    
+    def perform_create(self, serializer):
+        # Create new tag
+        serializer.save(user=self.request.user)
     
     
