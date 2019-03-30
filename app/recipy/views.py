603cdad2bcd64f8e7ca13f7bd20f115caf2cf43d
Zoran <zoranstoilov@yahoo.com>
2019-03-30 17:32:05 +0000

    queryset for authorized user

--- app/recipy/views.py
+++ app/recipy/views.py
@@ -9,6 +9,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     # Manage tags in the database
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
