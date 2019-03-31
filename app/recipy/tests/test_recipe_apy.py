d74c7034d5e41be6df73fe5de1fbddc7d7322a14
Zoran <zoranstoilov@yahoo.com>
2019-03-31 22:16:25 +0100

    123

--- app/recipy/tests/test_recipe_apy.py
+++ app/recipy/tests/test_recipe_apy.py
@@ -8,7 +8,7 @@ from rest_framework.test import APIClient
 from core.models import Recipe
 from recipy.serializers import RecipeSerializer
 
-RECIPES_URL = reverse('recipe:recipe-list')
+RECIPES_URL = reverse('recipy:recipe-list')
 
 
 def sample_recipe(user, **params):
